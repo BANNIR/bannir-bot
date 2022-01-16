396c4a61b83d8bc1cb80c0f5fffc0ca7f944bb7e
BANNIR <shahe.bannis@gmail.com>
2022-01-15 21:16:31 -0500

    basic slash commands

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config()
 
 const generateImage = require("./generateImage")
 
+
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
@@ -13,6 +15,42 @@ const client = new Discord.Client({
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
+
+    //making update local / global
+    const guildID = "796845537927102495"
+    const guild = client.guilds.cache.get(guildID)
+    let commmands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application.commands
+    }
+
+    //creating commands
+    commands.create({
+        name: 'ping',
+        description: 'Replies with pong'
+    })
+
+    commands.create({
+        name: 'add',
+        description: 'adds two numbers togheter.',
+        options: [
+            {
+                name: 'num1',
+                description: 'The first number',
+                required: true,
+                type: Discord.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'The second number',
+                required: true,
+                type: Discord.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    })
 })
 
 client.on("messageCreate", (message) => {
@@ -21,6 +59,7 @@ client.on("messageCreate", (message) => {
     }
 })
 
+//welcome message with photo
 // const generalChannelId = "796845538536456204"
 const welcomeChannelId = "931028269736812554"
 client.on("guildMemberAdd", async (member) => {
@@ -29,6 +68,36 @@ client.on("guildMemberAdd", async (member) => {
         content: `<@${member.id}> Welcome to the server!`,
         files: [img]
     })
+})
+
+//commands
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand) {
+        return
+    }
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'ping') {
+        interaction.reply({
+            content: 'pong',
+            ephemeral: true,
+        })
+    } else if (commandName === 'add') {
+        const num1 = options.getNumber('num1')
+        const num2 = options.getNumber('num2')
+
+        // set a wait time of 5 seconds
+        await interaction.deferReply({
+            ephemeral: true
+        })
+
+        await new Promise(resolve => setTimeout(resolve, 5000))
+
+        await interaction.editReply({
+            content: `The sum is ${num1 + num2}`,
+        })
+    }
 })
 
 client.login(process.env.TOKEN)
